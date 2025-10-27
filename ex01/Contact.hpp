e1aedcdd670cd604359228a212a1d47d04f514e9
Chloe Americo <camerico@f2r11s12.clusters.42paris.fr>
2025-10-27 16:40:07 +0100

    cpp01 en cours

---
+++ ex01/Contact.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 13:03:51 by camerico          #+#    #+#             */
+/*   Updated: 2025/10/27 14:33:06 by camerico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Déclaration de la classe Contact
+//une page du carnet
+
+/*	classe stocke les informations personnelles d'un contact, telles que
+*	le prénom, le nom, le surnom, le numéro de téléphone et le secret le plus 
+*	sombre.*/
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+
+#include <string>
+
+class Contact
+{
+	private:
+	std::string	_first_name;
+	std::string	_last_name;
+	std::string	_nickname;
+	std::string	_phone_number;
+	std::string	_darkest_secret;
+
+	public:
+	Contact();		//constructeur
+
+	void	setFirstName(std::string first_name);
+	void	setLastName(std::string last_name);
+	void	setNickname(std::string nickname);
+	void	setPhoneNumber(std::string phone_number);
+	void	setDarkestSecret(std::string darkest_secret);
+
+	//fonctions pour lire les attributs privés, mais sans les modifs
+	// avec le const, on ne modiifie rien
+
+	std::string getFirstName(void) const;
+	std::string	getLastName(void) const;
+	std::string	getNickname(void) const;
+	std::string	getPhoneNumber(void) const;
+	std::string	getDarkestSecret(void) const;
+}
+
+
+#endif
